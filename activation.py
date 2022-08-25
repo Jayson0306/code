3c913607a9002f803155d00a3cde03220c18c77f
Jayson0306 <83279649+Jayson0306@users.noreply.github.com>
2022-08-25 23:46:09 +0800

    Update activation.py

--- activation.py
+++ activation.py
@@ -1,30 +1,38 @@
 import torch
 import torch.nn as tn
 import torch.nn.functional as tnf
-from torch.nn.parameter import Parameter
 import numpy as np
 
 
-class my_actFunc(tn.Module):  # optional activation function 定义了可以选择的激活函数
+# optional activation function 定义了可以选择的激活函数
+class my_actFunc(tn.Module):
     def __init__(self, actName='linear'):
         super(my_actFunc, self).__init__()
-        self.actName = actName  # 默认值
+        self.actName = actName
 
     def forward(self, x_input):
         if str.lower(self.actName) == 'relu':
-            out_x = tnf.relu(x_input)  # 作一个relu的操作
+            out_x = tnf.relu(x_input)
         elif str.lower(self.actName) == 'leaky_relu':
             out_x = tnf.leaky_relu(x_input)
         elif str.lower(self.actName) == 'tanh':
             out_x = torch.tanh(x_input)
         elif str.lower(self.actName) == 'srelu':
             out_x = tnf.relu(x_input) * tnf.relu(1 - x_input)
+        elif str.lower(self.actName) == 's2relu':
+            out_x = tnf.relu(x_input) * tnf.relu(1 - x_input) * torch.sin(2 * np.pi * x_input)
         elif str.lower(self.actName) == 'elu':
             out_x = tnf.elu(x_input)
         elif str.lower(self.actName) == 'sin':
             out_x = torch.sin(x_input)
+        elif str.lower(self.actName) == 'sinaddcos':
+            out_x = 0.5 * torch.sin(x_input) + 0.5 * torch.cos(x_input)
+        elif str.lower(self.actName) == 'fourier':
+            out_x = torch.cat([torch.sin(x_input), torch.cos(x_input)], dim=-1)
         elif str.lower(self.actName) == 'sigmoid':
             out_x = tnf.sigmoid(x_input)
+        elif str.lower(self.actName) == 'gelu':
+            out_x = tnf.gelu(x_input)
         else:
             out_x = x_input
         return out_x
